5a578c4dc94fc4f66b2182e9cf2da26eda694655
brandon-wever <brandon.w3ver@gmail.com>
2023-03-02 16:47:39 -0500

    Added appearance style

---
+++ OCKSample/Main/Stylers/AppearanceStyle.swift
@@ -0,0 +1,15 @@
+//
+//  AppearanceStyle.swift
+//  OCKSample
+//
+//  Created by  on 3/2/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
+import CareKitUI
+
+struct AppearanceStyle: OCKAppearanceStyler {
+    var cornerRadius1: CGFloat { 50 }
+    var cornerRadius2: CGFloat { 50 }
+}
